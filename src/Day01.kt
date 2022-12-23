25ef615ac4d116339867601c3ce1225d8754282c
Jorge Díaz <jorge@fidel.uk>
2022-12-23 21:25:08 +0000

    + Add Day1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,38 @@
 fun main() {
+    fun caloriesByElf(input: List<String>): List<Int> {
+        val result: MutableList<MutableList<Int>> = mutableListOf()
+        var smallResult: MutableList<Int> = mutableListOf()
+
+        for ((index, calorie) in input.withIndex()) {
+            if (calorie == "") {
+                result.add(smallResult)
+                smallResult = mutableListOf()
+            } else {
+                smallResult.add(calorie.toInt())
+            }
+
+            if (index == input.lastIndex) {
+                result.add(smallResult)
+            }
+        }
+
+        return result.map { list -> list.reduce { acc, number -> acc + number } }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val calories = caloriesByElf(input).sorted().reversed()
+        return calories[0]
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val calories = caloriesByElf(input).sorted().reversed()
+        return calories[0] + calories[1] + calories[2]
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+
+    println(part1(input))
+    println(part2(input))
+    check(part1(input) == 70369)
+    check(part2(input) == 203002)
 }
